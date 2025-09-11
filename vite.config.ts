bc8e1d840b2e12772b5952596d3d150354f1c987
Jaekook Brian Lee <107831698+JaeBrian@users.noreply.github.com>
2025-09-11 06:24:04 -0700

    feat: vm-sdk endpoints with cloudflare (#71)
    * remove secrets, and local testings
    
    Signed-off-by: JaeBrian <bnlee419@gmail.com>
    
    * Remove environment files from tracking
    
    * add text input, and fix configs
    
    Signed-off-by: JaeBrian <bnlee419@gmail.com>
    
    * add correct kv namespace
    
    Signed-off-by: JaeBrian <bnlee419@gmail.com>
    
    * correct wrangler.json
    
    Signed-off-by: JaeBrian <bnlee419@gmail.com>
    
    * chore: remove .wrangler/tmp and add to git ignore
    
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>
    
    ---------
    
    Signed-off-by: JaeBrian <bnlee419@gmail.com>
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>
    Co-authored-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,42 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    wasm(),
+    topLevelAwait(),
+    react(), 
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@cardano-sdk/util/node_modules/serialize-error": "serialize-error",
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8788',
+        changeOrigin: true,
+      }
+    }
+  }
 })
